c7b5ea6e5177b4820c595f551047be293cf3ea66
david curtis <heythereimdavidcurtis@gmail.com>
2022-03-24 18:37:01 -0400

    added bcrypt password hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,8 +52,11 @@ router.post("/", (req, res) => {
 
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
-  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
