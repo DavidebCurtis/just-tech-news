5df094749a966fc1d60d37b46bccd7d1a7b08748
Michael Curtis <mpbcurtis@gmail.com>
2022-03-26 14:38:33 -0400

    app name change

--- src/config/connection.js
+++ src/config/connection.js
@@ -6,7 +6,7 @@ const config = require(__dirname + "/../config/config.js")[env];
 // create connection to our db
 if (process.env.NODE_ENV === "production") {
   var sequelize = new Sequelize(config.db_connection, config);
-} else {
+} else if (process.env.NODE_ENV === "development") {
   console.log("DATABASE", config.database);
   console.log("username", config.username);
   console.log("password", config.password);
@@ -20,6 +20,20 @@ if (process.env.NODE_ENV === "production") {
       dialectOptions: {
         socketPath: "/tmp/mysql.sock"
       },
+      port: config.port
+    }
+  );
+} else {
+  console.log("DATABASE", config.database);
+  console.log("username", config.username);
+  console.log("password", config.password);
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      host: config.host,
+      dialect: config.dialect,
       port: config.port
     }
   );
