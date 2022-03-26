3be0b1f9595323aa5117133ef6e71dda13c68862
david curtis <heythereimdavidcurtis@gmail.com>
2022-03-26 11:51:52 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -9,18 +9,19 @@ require("dotenv").config();
 
 // }
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  null,
-  {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
     host: "localhost",
     dialect: "mysql",
     dialectOptions: {
       socketPath: "/tmp/mysql.sock",
     },
     port: 3306,
-  }
-);
+  });
+}
 
 module.exports = sequelize;
